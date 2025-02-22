015d4af4ffa46a240c523dfa53843cf343fd6ec3
pritam dutta <webdevpritam7498@gmail.com>
2025-02-22 22:28:20 +0600

    creating a basic navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +28,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
-        {children}
+        <header>
+          <nav className="bg-slate-700  text-zinc-50 w-full h-16 flex justify-between items-center px-20">
+            <Link href="/">Home</Link>
+
+            <Link href="/dashboard">Dashboard</Link>
+          </nav>
+        </header>
+        <main>{children}</main>
+
+        <footer className="absolute  bottom-0 text-center bg-slate-800  text-zinc-50 w-full h-8 " >
+          <h6 className="text-xs text-center mt-2">@2025 All rights reserved.pritam</h6>
+        </footer>
       </body>
     </html>
   );
--- src/app/performance/page.tsx
+++
@@ -1,9 +0,0 @@
-const page = () => {
-return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900">
-        <div className="text-white text-center text-7xl font-bold">My Performance Page</div>
-    </div>
-);
-};
-
-export default page;
